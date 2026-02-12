b53ac8d4e9414ee36bb1067571064385bb476e8d
Sean <13078@holbertonstudents.com>
2026-02-12 11:39:37 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+* main - Entry point of program
+* Return: 0 if success
+*
+*/
+int main(void)
+{
+	int number;
+
+	for (number = 0 ; number <= 9 ; number++)
+	{
+	printf("%d", number);
+	}
+	printf("\n");
+
+	return (0);
+}
