aaa860195ba8577c48283fabb165e9f831e76c2e
Sean <13078@holbertonstudents.com>
2026-02-14 04:47:50 +0000

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+* main - Entry point of program
+* Return: 0 if success
+*
+*/
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
