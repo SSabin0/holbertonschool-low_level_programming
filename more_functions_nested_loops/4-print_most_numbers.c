83bae3c1cc3ac02cdc9f78243401623035766a65
Sean <13078@holbertonstudents.com>
2026-02-19 12:04:06 +0000

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_most_numbers -  prints the numbers except 2 and 4.
+*/
+
+void print_most_numbers(void)
+{
+
+char c;
+
+for (i = '0'; i < '9'; i++)
+{
+if (i != 2 && != 4)
+{
+putchar(c);
+}
+}
+putchar('\n');
+}
